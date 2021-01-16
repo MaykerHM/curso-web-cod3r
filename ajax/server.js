b3e74fd7d26a80d88e6447e352c9d3c01ac31d4b
MaykerHM <hydeki8@gmail.com>
2021-01-16 17:42:51 -0300

    exercicios

--- ajax/server.js
+++ ajax/server.js
@@ -2,9 +2,16 @@ const bodyParser = require('body-parser')
 const express = require('express')
 const app = express()
 
-
 app.use(express.static('.'))
-app.use(bodyParser.urlencoded({extended: true}))
+app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+const multer = require('multer')
+
+const storage = multer.diskStorage({
+  destination: function (req, file, useCallback) {
+    callback(null, '.')
+  },
+})
 
 app.listen(8080, () => console.log('Executando...'))
