47e53056fd3ece9d0fc1907668676f8eacd12f95
MaykerHM <hydeki8@gmail.com>
2021-01-20 19:33:37 -0300

    exercicios

--- ajax/server.js
+++ ajax/server.js
@@ -9,9 +9,24 @@ app.use(bodyParser.json())
 const multer = require('multer')
 
 const storage = multer.diskStorage({
-  destination: function (req, file, useCallback) {
-    callback(null, '.')
+  destination: function (req, file, callback) {
+    callback(null, './upload')
   },
+  filename: function (req, file, callback) {
+    callback(null, `${Date.now()}_${file.originalname}`)
+  },
+})
+
+const upload = multer({ storage }).single('arquivo')
+
+app.post('/upload', (req, res) => {
+  upload(req, res, (err) => {
+    if (err) {
+      return res.end('Ocorreu um erro.')
+    }
+
+    res.end('Concluído com sucesso.')
+  })
 })
 
 app.listen(8080, () => console.log('Executando...'))
